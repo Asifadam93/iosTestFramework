485bbbc3a394815df026e1f49c915f53ba340a0f
Asif <adamsha.asif@gmail.com>
2017-01-25 12:05:36 +0100

    Initial project commit

---
+++ iosFramework/iosFramework.h
@@ -0,0 +1,19 @@
+//
+//  iosFramework.h
+//  iosFramework
+//
+//  Created by Asif on 25/01/2017.
+//  Copyright © 2017 Asif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iosFramework.
+FOUNDATION_EXPORT double iosFrameworkVersionNumber;
+
+//! Project version string for iosFramework.
+FOUNDATION_EXPORT const unsigned char iosFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iosFramework/PublicHeader.h>
+
+
